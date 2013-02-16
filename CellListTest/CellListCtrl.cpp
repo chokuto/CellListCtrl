2280efe730d6a81c1442820f2c73878cf156d202
chokuto <chokutto@gmail.com>
2013-02-16 16:57:08 +0900

    ウィンドウクラス登録処理追加

--- CellListTest/CellListCtrl.cpp
+++ CellListTest/CellListCtrl.cpp
@@ -2,9 +2,7 @@
 //
 
 #include "stdafx.h"
-#include "CellListTest.h"
 #include "CellListCtrl.h"
-
 
 // CCellListCtrl
 
@@ -12,7 +10,9 @@ IMPLEMENT_DYNAMIC(CCellListCtrl, CWnd)
 
 CCellListCtrl::CCellListCtrl()
 {
-
+	if (!RegisterWindowClass()) {
+		AfxThrowResourceException();
+	}
 }
 
 CCellListCtrl::~CCellListCtrl()
@@ -27,4 +27,30 @@ END_MESSAGE_MAP()
 
 // CCellListCtrl メッセージ ハンドラー
 
+// コントロールのウィンドウクラス登録
+bool CCellListCtrl::RegisterWindowClass()
+{
+	HINSTANCE hInstance = AfxGetInstanceHandle();
+	WNDCLASS wndclass = {};
+	if (::GetClassInfo(hInstance, CELLLISTCTRL_CLASSNAME, &wndclass)) {
+		// 既に登録済みなら何もしない
+		return true;
+	}
 
+	wndclass.style = CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = ::DefWindowProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = NULL;
+	wndclass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_3DFACE + 1);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = CELLLISTCTRL_CLASSNAME;
+
+	if (!AfxRegisterClass(&wndclass)) {
+		return false;
+	}
+
+	return true;
+}
--- CellListTest/CellListCtrl.h
+++ CellListTest/CellListCtrl.h
@@ -1,8 +1,11 @@
 #pragma once
 
-
 // CCellListCtrl
 
+// コントロールのウィンドウクラス名
+const LPCTSTR CELLLISTCTRL_CLASSNAME = _T("CellListCtrl");
+
+// カスタムコントロールクラス定義
 class CCellListCtrl : public CWnd
 {
 	DECLARE_DYNAMIC(CCellListCtrl)
@@ -13,6 +16,9 @@ public:
 
 protected:
 	DECLARE_MESSAGE_MAP()
+
+private:
+	bool RegisterWindowClass();
 };
 
 
