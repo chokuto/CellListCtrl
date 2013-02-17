f9cf3c9fd1d60ea5fa561f4e7e6e078bf48a3a2a
chokuto <chokutto@gmail.com>
2013-02-17 16:22:04 +0900

    GoogleTestフレームワーク向け単体テストプロジェクトを追加

---
+++ CellListDataSourceTest/CellListDataSourceTest.cpp
@@ -0,0 +1,50 @@
+// CellListDataSourceTest.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "CellListDataSourceTest.h"
+#include "gtest/gtest.h"
+#include "../CellListCtrl/CellListCtrl.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 唯一のアプリケーション オブジェクトです。
+
+CWinApp theApp;
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(NULL);
+
+	if (hModule != NULL)
+	{
+		// MFC を初期化して、エラーの場合は結果を印刷します。
+		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+		{
+			// TODO: 必要に応じてエラー コードを変更してください。
+			_tprintf(_T("致命的なエラー: MFC の初期化ができませんでした。\n"));
+			nRetCode = 1;
+		}
+		else
+		{
+			// TODO: アプリケーションの動作を記述するコードをここに挿入してください。
+			::testing::InitGoogleTest(&argc, argv);
+			nRetCode = RUN_ALL_TESTS();
+		}
+	}
+	else
+	{
+		// TODO: 必要に応じてエラー コードを変更してください。
+		_tprintf(_T("致命的なエラー: GetModuleHandle が失敗しました\n"));
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
